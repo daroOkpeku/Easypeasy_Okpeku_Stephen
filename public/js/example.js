aafaca89c542f7a3a6f023f811e225492a0efb5f
si.okpeku@otandtconsulting.com <tempdev07@swglobal.com>
2024-11-12 14:25:59 +0100

    update

--- public/js/example.js
+++ public/js/example.js
@@ -1,14 +1,23 @@
 const wholecover = document.querySelector(".wholecover")
+const prev = document.querySelector(".prev")
+const next = document.querySelector(".next")
+const status_select = document.querySelector(".status_select")
+const priority_select = document.querySelector(".priority_select")
+let total_num = 0;
+let current = 1;
 let original = window.location.origin
 let headers = new Headers();
-
+let ansstatus_select = ""
+let anspriority_select = ""
 let url = `${original}/fetchall`;
-//let headers = new Headers();
 axios.get(url,  headers).then(res=>{
  if(res.data.success){
+    total_num = res.data.success.last_page
+
    const data = res.data.success.data
+   console.log(res.data.success)
    data.map((item, index)=>{
-    
+
     const date = new Date(item.created_at);
     const humanReadableDate = date.toLocaleString("en-US", {
     weekday: "long",      // e.g., 'Tuesday'
@@ -36,10 +45,417 @@ axios.get(url,  headers).then(res=>{
                 <td class="text-sm font-medium capitelize text-center" >
                 ${humanReadableDate}
                 </td>
-           
+
              </tr>`;
    })
 
  }
 
+})
+
+
+status_select.addEventListener("click",function(e){
+    e.preventDefault()
+   
+    if(e.target.value != 'Select Status'){
+        ansstatus_select = e.target.value
+         wholecover.innerHTML = ''
+        //  status         
+        let url = `${original}/fetchall?status=${e.target.value}`;
+axios.get(url,  headers).then(res=>{
+ if(res.data.success){
+    total_num = res.data.success.last_page
+
+   const data = res.data.success.data
+   console.log(res.data.success)
+   data.map((item, index)=>{
+
+    const date = new Date(item.created_at);
+    const humanReadableDate = date.toLocaleString("en-US", {
+    weekday: "long",      // e.g., 'Tuesday'
+    year: "numeric",      // e.g., 2024
+    month: "long",        // e.g., 'October'
+    day: "numeric",       // e.g., 29
+    hour: "numeric",      // e.g., 3 PM or 15 in 24-hour format
+    minute: "2-digit"     // e.g., 26
+    });
+    wholecover.innerHTML += `
+                <tr class="text-black" >
+                <td class="text-sm font-medium capitelize text-center" >${item.company} </td>
+                <td class="text-sm font-medium capitelize text-center" >
+                ${item.title}
+                </td>
+                <td class="text-sm font-medium capitelize text-center" >
+                ${item.priority}
+                </td>
+                <td class="text-sm font-medium capitelize text-center" >
+                  ${item.status}
+                </td>
+                <td class="text-sm font-medium capitelize text-center" >
+                ${item.to_age+"-"+item.from_age}
+                </td>
+                <td class="text-sm font-medium capitelize text-center" >
+                ${humanReadableDate}
+                </td>
+
+             </tr>`;
+   })
+
+ }
+
+})
+    }else{
+         wholecover.innerHTML = ''
+        let url = `${original}/fetchall`;
+axios.get(url,  headers).then(res=>{
+ if(res.data.success){
+    total_num = res.data.success.last_page
+
+   const data = res.data.success.data
+   console.log(res.data.success)
+   data.map((item, index)=>{
+
+    const date = new Date(item.created_at);
+    const humanReadableDate = date.toLocaleString("en-US", {
+    weekday: "long",      // e.g., 'Tuesday'
+    year: "numeric",      // e.g., 2024
+    month: "long",        // e.g., 'October'
+    day: "numeric",       // e.g., 29
+    hour: "numeric",      // e.g., 3 PM or 15 in 24-hour format
+    minute: "2-digit"     // e.g., 26
+    });
+    wholecover.innerHTML += `
+                <tr class="text-black" >
+                <td class="text-sm font-medium capitelize text-center" >${item.company} </td>
+                <td class="text-sm font-medium capitelize text-center" >
+                ${item.title}
+                </td>
+                <td class="text-sm font-medium capitelize text-center" >
+                ${item.priority}
+                </td>
+                <td class="text-sm font-medium capitelize text-center" >
+                  ${item.status}
+                </td>
+                <td class="text-sm font-medium capitelize text-center" >
+                ${item.to_age+"-"+item.from_age}
+                </td>
+                <td class="text-sm font-medium capitelize text-center" >
+                ${humanReadableDate}
+                </td>
+
+             </tr>`;
+   })
+
+ }
+
+})
+    }
+})
+
+priority_select.addEventListener("click", function(e){
+    e.preventDefault()
+    if(e.target.value != 'Select priority'){
+        anspriority_select = e.target.value
+        wholecover.innerHTML = ''
+
+        let url = `${original}/fetchall?priority=${e.target.value}`;
+        axios.get(url,  headers).then(res=>{
+         if(res.data.success){
+            total_num = res.data.success.last_page
+        
+           const data = res.data.success.data
+           console.log(res.data.success)
+           data.map((item, index)=>{
+        
+            const date = new Date(item.created_at);
+            const humanReadableDate = date.toLocaleString("en-US", {
+            weekday: "long",      // e.g., 'Tuesday'
+            year: "numeric",      // e.g., 2024
+            month: "long",        // e.g., 'October'
+            day: "numeric",       // e.g., 29
+            hour: "numeric",      // e.g., 3 PM or 15 in 24-hour format
+            minute: "2-digit"     // e.g., 26
+            });
+            wholecover.innerHTML += `
+                        <tr class="text-black" >
+                        <td class="text-sm font-medium capitelize text-center" >${item.company} </td>
+                        <td class="text-sm font-medium capitelize text-center" >
+                        ${item.title}
+                        </td>
+                        <td class="text-sm font-medium capitelize text-center" >
+                        ${item.priority}
+                        </td>
+                        <td class="text-sm font-medium capitelize text-center" >
+                          ${item.status}
+                        </td>
+                        <td class="text-sm font-medium capitelize text-center" >
+                        ${item.to_age+"-"+item.from_age}
+                        </td>
+                        <td class="text-sm font-medium capitelize text-center" >
+                        ${humanReadableDate}
+                        </td>
+        
+                     </tr>`;
+           })
+        
+         }
+        
+        })
+    }else{
+         wholecover.innerHTML = ''
+        let url = `${original}/fetchall`;
+axios.get(url,  headers).then(res=>{
+ if(res.data.success){
+    total_num = res.data.success.last_page
+
+   const data = res.data.success.data
+   console.log(res.data.success)
+   data.map((item, index)=>{
+
+    const date = new Date(item.created_at);
+    const humanReadableDate = date.toLocaleString("en-US", {
+    weekday: "long",      // e.g., 'Tuesday'
+    year: "numeric",      // e.g., 2024
+    month: "long",        // e.g., 'October'
+    day: "numeric",       // e.g., 29
+    hour: "numeric",      // e.g., 3 PM or 15 in 24-hour format
+    minute: "2-digit"     // e.g., 26
+    });
+    wholecover.innerHTML += `
+                <tr class="text-black" >
+                <td class="text-sm font-medium capitelize text-center" >${item.company} </td>
+                <td class="text-sm font-medium capitelize text-center" >
+                ${item.title}
+                </td>
+                <td class="text-sm font-medium capitelize text-center" >
+                ${item.priority}
+                </td>
+                <td class="text-sm font-medium capitelize text-center" >
+                  ${item.status}
+                </td>
+                <td class="text-sm font-medium capitelize text-center" >
+                ${item.to_age+"-"+item.from_age}
+                </td>
+                <td class="text-sm font-medium capitelize text-center" >
+                ${humanReadableDate}
+                </td>
+
+             </tr>`;
+   })
+
+ }
+
+})
+    }
+})
+
+
+prev.addEventListener("click", function(e){
+    e.preventDefault();
+    if (current > 1){
+        let ans =  current -= 1;
+        current = ans
+        wholecover.innerHTML = ''
+        console.log(current)
+
+
+        let url = `${original}/fetchall?page=${current}&status=${ansstatus_select}&priority=${anspriority_select}`;
+axios.get(url,  headers).then(res=>{
+ if(res.data.success){
+    total_num = res.data.success.last_page
+
+   const data = res.data.success.data
+   console.log(res.data.success)
+   data.map((item, index)=>{
+
+    const date = new Date(item.created_at);
+    const humanReadableDate = date.toLocaleString("en-US", {
+    weekday: "long",      // e.g., 'Tuesday'
+    year: "numeric",      // e.g., 2024
+    month: "long",        // e.g., 'October'
+    day: "numeric",       // e.g., 29
+    hour: "numeric",      // e.g., 3 PM or 15 in 24-hour format
+    minute: "2-digit"     // e.g., 26
+    });
+    wholecover.innerHTML += `
+                <tr class="text-black" >
+                <td class="text-sm font-medium capitelize text-center" >${item.company} </td>
+                <td class="text-sm font-medium capitelize text-center" >
+                ${item.title}
+                </td>
+                <td class="text-sm font-medium capitelize text-center" >
+                ${item.priority}
+                </td>
+                <td class="text-sm font-medium capitelize text-center" >
+                  ${item.status}
+                </td>
+                <td class="text-sm font-medium capitelize text-center" >
+                ${item.to_age+"-"+item.from_age}
+                </td>
+                <td class="text-sm font-medium capitelize text-center" >
+                ${humanReadableDate}
+                </td>
+
+             </tr>`;
+   })
+
+ }
+
+})
+
+
+    }
+    else if(current == 1 ){
+        current = 1
+        wholecover.innerHTML = ''
+        console.log(current)
+
+
+        let url = `${original}/fetchall?page=${current}&status=${ansstatus_select}&priority=${anspriority_select}`;
+        axios.get(url,  headers).then(res=>{
+         if(res.data.success){
+            total_num = res.data.success.last_page
+
+           const data = res.data.success.data
+           console.log(res.data.success)
+           data.map((item, index)=>{
+
+            const date = new Date(item.created_at);
+            const humanReadableDate = date.toLocaleString("en-US", {
+            weekday: "long",      // e.g., 'Tuesday'
+            year: "numeric",      // e.g., 2024
+            month: "long",        // e.g., 'October'
+            day: "numeric",       // e.g., 29
+            hour: "numeric",      // e.g., 3 PM or 15 in 24-hour format
+            minute: "2-digit"     // e.g., 26
+            });
+            wholecover.innerHTML += `
+                        <tr class="text-black" >
+                        <td class="text-sm font-medium capitelize text-center" >${item.company} </td>
+                        <td class="text-sm font-medium capitelize text-center" >
+                        ${item.title}
+                        </td>
+                        <td class="text-sm font-medium capitelize text-center" >
+                        ${item.priority}
+                        </td>
+                        <td class="text-sm font-medium capitelize text-center" >
+                          ${item.status}
+                        </td>
+                        <td class="text-sm font-medium capitelize text-center" >
+                        ${item.to_age+"-"+item.from_age}
+                        </td>
+                        <td class="text-sm font-medium capitelize text-center" >
+                        ${humanReadableDate}
+                        </td>
+
+                     </tr>`;
+           })
+
+         }
+
+        })
+    }
+})
+
+
+
+next.addEventListener("click", function(e){
+    e.preventDefault();
+    if(current < total_num){
+        let ans =  current += 1;
+        current = ans
+        wholecover.innerHTML = ''
+        console.log(current)
+        let url = `${original}/fetchall?page=${current}&status=${ansstatus_select}&priority=${anspriority_select}`;
+        axios.get(url,  headers).then(res=>{
+         if(res.data.success){
+            total_num = res.data.success.last_page
+
+           const data = res.data.success.data
+           console.log(res.data.success)
+           data.map((item, index)=>{
+
+            const date = new Date(item.created_at);
+            const humanReadableDate = date.toLocaleString("en-US", {
+            weekday: "long",      // e.g., 'Tuesday'
+            year: "numeric",      // e.g., 2024
+            month: "long",        // e.g., 'October'
+            day: "numeric",       // e.g., 29
+            hour: "numeric",      // e.g., 3 PM or 15 in 24-hour format
+            minute: "2-digit"     // e.g., 26
+            });
+            wholecover.innerHTML += `
+                        <tr class="text-black" >
+                        <td class="text-sm font-medium capitelize text-center" >${item.company} </td>
+                        <td class="text-sm font-medium capitelize text-center" >
+                        ${item.title}
+                        </td>
+                        <td class="text-sm font-medium capitelize text-center" >
+                        ${item.priority}
+                        </td>
+                        <td class="text-sm font-medium capitelize text-center" >
+                          ${item.status}
+                        </td>
+                        <td class="text-sm font-medium capitelize text-center" >
+                        ${item.to_age+"-"+item.from_age}
+                        </td>
+                        <td class="text-sm font-medium capitelize text-center" >
+                        ${humanReadableDate}
+                        </td>
+
+                     </tr>`;
+           })
+
+         }
+
+        })
+    } else if(current == total_num){
+        current = 1
+        wholecover.innerHTML = ''
+        console.log(current)
+
+        let url = `${original}/fetchall?page=${current}&status=${ansstatus_select}&priority=${anspriority_select}`;
+        axios.get(url,  headers).then(res=>{
+         if(res.data.success){
+            total_num = res.data.success.last_page
+
+           const data = res.data.success.data
+           console.log(res.data.success)
+           data.map((item, index)=>{
+
+            const date = new Date(item.created_at);
+            const humanReadableDate = date.toLocaleString("en-US", {
+            weekday: "long",      // e.g., 'Tuesday'
+            year: "numeric",      // e.g., 2024
+            month: "long",        // e.g., 'October'
+            day: "numeric",       // e.g., 29
+            hour: "numeric",      // e.g., 3 PM or 15 in 24-hour format
+            minute: "2-digit"     // e.g., 26
+            });
+            wholecover.innerHTML += `
+                        <tr class="text-black" >
+                        <td class="text-sm font-medium capitelize text-center" >${item.company} </td>
+                        <td class="text-sm font-medium capitelize text-center" >
+                        ${item.title}
+                        </td>
+                        <td class="text-sm font-medium capitelize text-center" >
+                        ${item.priority}
+                        </td>
+                        <td class="text-sm font-medium capitelize text-center" >
+                          ${item.status}
+                        </td>
+                        <td class="text-sm font-medium capitelize text-center" >
+                        ${item.to_age+"-"+item.from_age}
+                        </td>
+                        <td class="text-sm font-medium capitelize text-center" >
+                        ${humanReadableDate}
+                        </td>
+
+                     </tr>`;
+           })
+
+         }
+
+        })
+    }
 })
--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -6,13 +6,39 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <script src="https://cdn.tailwindcss.com"></script>
     <meta name="csrf-token" content="{{ csrf_token() }}">
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/js/all.min.js" integrity="sha512-6sSYJqDreZRZGkJ3b+YfdhB3MzmuP9R7X1QZ6g5aIXhRvR1Y/N/P47jmnkENm7YL3oqsmI6AK+V6AD99uWDnIw==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/css/brands.min.css" integrity="sha512-EJp8vMVhYl7tBFE2rgNGb//drnr1+6XKMvTyamMS34YwOEFohhWkGq13tPWnK0FbjSS6D8YoA3n3bZmb3KiUYA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     <title>Document</title>
 </head>
 <body>
 
+    <div class="w-11/12 m-auto flex flex-col gap-2 sm:flex sm:flex-col sm:gap-2  md:flex md:flex-row md:gap-2 lg:flex lg:flex-row lg:gap-2 px-3 py-2 mt-5">
+        <section class="w-1/5 flex flex-col items-center">
+        <div class="w-full">Status</div>
+        <div class="w-full">
+            <select class="w-fulll border status_select">
+                <option class="text-xs">Select Status</option>
+                <option class="text-xs">active</option>
+                <option class="text-xs">inactive</option>
+            </select>
+        </div>
+        </section>
 
-    <div class='overflow-x-auto  overflow-hidden mt-20 w-11/12 m-auto rounded-l-md rounded-r-md'>
-        <table class='w-full '>
+        <section class="w-1/5 flex flex-col items-center">
+            <div class="w-full">priority</div>
+            <div class="w-full">
+                <select class="w-fulll border priority_select">
+                    <option class="text-xs">Select priority</option>
+                    <option class="text-xs">low</option>
+                    <option class="text-xs">medium</option>
+                    <option class="text-xs">high</option>
+                </select>
+            </div>
+            </section>
+    </div>
+
+    <div class='overflow-scroll sm:overflow-scroll md:overflow-hidden lg:overflow-scroll mt-20 w-full sm:w-full md:w-11/12 lg:w-11/12 m-auto rounded-l-md rounded-r-md'>
+        <table class='w-max sm:w-max md:w-full lg:w-full border-2'>
         <thead >
            <tr class="  rounded-r-md" >
            <th class='text-white bg-green-400 border border-b-2 font-medium  capitalize text-[8px] sm:text-xs md:text-sm lg:text-base text-center whitespace-nowrap'>company</th>
@@ -25,17 +51,27 @@
         </thead>
 
         <tbody class="wholecover">
-           
-               
-          
+
+
+
         </tbody>
 
        </table>
+
        </div>
 
+       <div class="w-full justify-center items-center">
+        <article class="w-1/3 flex flex-row items-center justify-center m-auto space-x-2">
+         <button class="prev"><i class="fa fa-angle-double-left" aria-hidden="true"></i></button>
+         <button class="next"><i class="fa fa-angle-double-right" aria-hidden="true"></i></button>
+     </article>
+    </div>
 
 
-    
+
+
+
+
 </body>
 <script src="https://cdn.jsdelivr.net/npm/axios/dist/axios.min.js"></script>
 <script type="text/javascript">
